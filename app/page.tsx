ff83291bdd09b23a755db91380e642c82846e4f5
BuildTools <unconfigured@null.spigotmc.org>
2023-12-14 15:13:51 -0600

    added dynamic date

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { TopicCard } from "@/components/topic-card";
+import { format } from "date-fns";
 
 export default function Home() {
   return (
     <main className="flex flex-col p-24">
-      <h1 className="text-3xl font-bold mb-8">Wednesday, Dec 23</h1>
+      <h1 className="text-3xl font-bold mb-8">{format(new Date(), 'EEEE, LLL Mo')}</h1>
       <div className="flex flex-col gap-4">
         <TopicCard />
       </div>
